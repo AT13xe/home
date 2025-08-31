cddc2144bb6b2ac19e4cd9dd6d991c1de593583a
AT13xe <845043294@qq.com>
2025-08-31 23:14:56 +0800

    Update Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -39,7 +39,7 @@ const bgRandom = Math.floor(Math.random() * 10 + 1);
 // 更换壁纸链接
 const changeBg = (type) => {
   if (type == 0) {
-    bgUrl.value = `/images/background${bgRandom}.png`;
+    bgUrl.value = `/images/background${bgRandom}.jpg`;
   }
 };
 
